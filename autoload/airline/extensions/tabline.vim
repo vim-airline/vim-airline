vim-airline/vim-airline

commit e4ef624ea88858ebf8b7dc88d1aafb6f75d904fb

tabline for combined buffers/tabs
This is a new tabline extension, that displays both the buffers open and
the available tabs. This has been requested by #639 and fixes #639.

This is based on blings work on branch spike.

This uses the new highlight groups tab*_right, so that the separators
have the correct color. Also this makes some configuration variable
obsolete and therefore, they have been removed.

remove unused combined config variable, remove space after tab

autoload/airline/extensions/tabline.vim
@@ -5,6 +5,7 @@ let s:formatter = get(g:, 'airline#extensions#tabline#formatter', 'default')
 let s:show_buffers = get(g:, 'airline#extensions#tabline#show_buffers', 1)
 let s:show_tabs = get(g:, 'airline#extensions#tabline#show_tabs', 1)
 let s:ignore_bufadd_pat = get(g:, 'airline#extensions#tabline#ignore_bufadd_pat', '\c\vgundo|undotree|vimfiler|tagbar|nerd_tree')
+
 let s:taboo = get(g:, 'airline#extensions#taboo#enabled', 1) && get(g:, 'loaded_taboo', 0)
 if s:taboo
   let g:taboo_tabline = 0
@@ -47,7 +48,7 @@ function! s:update_tabline()
   elseif !get(g:, 'airline#extensions#tabline#enabled', 0)
     return
   " return, if buffer matches ignore pattern or is directory (netrw)
-  elseif empty(match) 
+  elseif empty(match)
         \ || match(match, s:ignore_bufadd_pat) > -1
         \ || isdirectory(expand("<afile>"))
     return
@@ -91,6 +92,7 @@ function! airline#extensions#tabline#load_theme(palette)
   call airline#highlighter#exec('airline_tabhid', l:tabhid)
 
   " Theme for tabs on the right
+  let l:tab_right     = get(colors, 'airline_tab_right',    a:palette.normal.airline_b)
   let l:tabsel_right  = get(colors, 'airline_tabsel_right', a:palette.normal.airline_a)
   let l:tabmod_right  = get(colors, 'airline_tabmod_right', a:palette.insert.airline_a)
   let l:tabhid_right  = get(colors, 'airline_tabhid_right', a:palette.normal.airline_c)
@@ -100,6 +102,7 @@ function! airline#extensions#tabline#load_theme(palette)
     "Fall back to normal airline_c if modified airline_c isn't present
     let l:tabmodu_right = get(colors, 'airline_tabmod_unsel_right', a:palette.normal.airline_c)
   endif
+  call airline#highlighter#exec('airline_tab_right',    l:tab_right)
   call airline#highlighter#exec('airline_tabsel_right', l:tabsel_right)
   call airline#highlighter#exec('airline_tabmod_right', l:tabmod_right)
   call airline#highlighter#exec('airline_tabhid_right', l:tabhid_right)
@@ -162,4 +165,25 @@ function! airline#extensions#tabline#new_builder()
   endif
 
   return airline#builder#new(builder_context)
+endfunction
+
+function! airline#extensions#tabline#group_of_bufnr(tab_bufs, bufnr)
+  let cur = bufnr('%')
+  if cur == a:bufnr
+    if g:airline_detect_modified && getbufvar(a:bufnr, '&modified')
+      let group = 'airline_tabmod'
+    else
+      let group = 'airline_tabsel'
+    endif
+    let s:current_modified = (group == 'airline_tabmod') ? 1 : 0
+  else
+    if g:airline_detect_modified && getbufvar(a:bufnr, '&modified')
+      let group = 'airline_tabmod_unsel'
+    elseif index(a:tab_bufs, a:bufnr) > -1
+      let group = 'airline_tab'
+    else
+      let group = 'airline_tabhid'
+    endif
+  endif
+  return group
 endfunction

autoload/airline/extensions/tabline/buffers.vim
@@ -62,23 +62,7 @@ function! airline#extensions#tabline#buffers#get()
       continue
     endif
 
-    if cur == nr
-      if g:airline_detect_modified && getbufvar(nr, '&modified')
-        let group = 'airline_tabmod'
-      else
-        let group = 'airline_tabsel'
-      endif
-      let s:current_modified = (group == 'airline_tabmod') ? 1 : 0
-    else
-      if g:airline_detect_modified && getbufvar(nr, '&modified')
-        let group = 'airline_tabmod_unsel'
-      elseif index(tab_bufs, nr) > -1
-        let group = 'airline_tab'
-      else
-        let group = 'airline_tabhid'
-      endif
-    endif
-
+    let group = airline#extensions#tabline#group_of_bufnr(tab_bufs, nr)
     if s:buffer_idx_mode
       if len(s:number_map) > 0
         call b.add_section(group, s:spc . get(s:number_map, l:index, '') . '%(%{airline#extensions#tabline#get_buffer_name('.nr.')}%)' . s:spc)

autoload/airline/extensions/tabline/tabs.vim
@@ -1,11 +1,9 @@
 " MIT License. Copyright (c) 2013-2016 Bailey Ling.
 " vim: et ts=2 sts=2 sw=2
 
-let s:show_tab_nr = get(g:, 'airline#extensions#tabline#show_tab_nr', 1)
-let s:tab_nr_type = get(g:, 'airline#extensions#tabline#tab_nr_type', 0)
 let s:show_close_button = get(g:, 'airline#extensions#tabline#show_close_button', 1)
-let s:show_tab_type = get(g:, 'airline#extensions#tabline#show_tab_type', 1)
 let s:close_symbol = get(g:, 'airline#extensions#tabline#close_symbol', 'X')
+let s:spc = g:airline_symbols.space
 
 let s:current_bufnr = -1
 let s:current_tabnr = -1
@@ -39,42 +37,39 @@ function! airline#extensions#tabline#tabs#get()
   endif
 
   let b = airline#extensions#tabline#new_builder()
+
+  let buffers = tabpagebuflist(curtab)
+  for nr in buffers
+    let group = airline#extensions#tabline#group_of_bufnr(buffers, nr)
+    call b.add_section_spaced(group, '%(%{airline#extensions#tabline#get_buffer_name('.nr.')}%)')
+  endfor
+  " truncate
+  call b.add_raw('%<')
+  call b.add_section('airline_tabfill', '')
+  call b.split()
+  call b.add_section('airline_tabfill', '')
+
   for i in range(1, tabpagenr('$'))
     if i == curtab
-      let group = 'airline_tabsel'
+      let group = 'airline_tabsel_right'
       if g:airline_detect_modified
         for bi in tabpagebuflist(i)
           if getbufvar(bi, '&modified')
-            let group = 'airline_tabmod'
+            let group = 'airline_tabmod_right'
           endif
         endfor
       endif
-      let s:current_modified = (group == 'airline_tabmod') ? 1 : 0
+      let s:current_modified = (group == 'airline_tabmod_right') ? 1 : 0
     else
-      let group = 'airline_tab'
+      let group = 'airline_tab_right'
     endif
-    let val = '%('
-    if s:show_tab_nr
-      if s:tab_nr_type == 0
-        let val .= (g:airline_symbols.space).'%{len(tabpagebuflist('.i.'))}'
-      elseif s:tab_nr_type == 1
-        let val .= (g:airline_symbols.space).i
-      else "== 2
-        let val .= (g:airline_symbols.space).i.'.%{len(tabpagebuflist('.i.'))}'
-      endif
-    endif
-    call b.add_section(group, val.'%'.i.'T %{airline#extensions#tabline#title('.i.')} %)')
+    call b.add_section(group, '%'.i.'Ttab '.i)
   endfor
+  call b.add_raw('%<')
 
-  call b.add_raw('%T')
-  call b.add_section('airline_tabfill', '')
-  call b.split()
-  if s:show_close_button
-    call b.add_section('airline_tab', ' %999X'.s:close_symbol.' ')
-  endif
-  if s:show_tab_type
-    call b.add_section('airline_tabtype', ' tabs ')
-  endif
+   if s:show_close_button
+     call b.add_section('airline_tab_right', ' %999X'.s:close_symbol.' ')
+   endif
 
   let s:current_bufnr = curbuf
   let s:current_tabnr = curtab

commit 63f9abd3f245b1d241c1a6f68f912b9188483eed

Fix highlighting of current/modified buffer.
Fixes broken variable reference caused by e4ef624 (#952).

autoload/airline/extensions/tabline.vim
@@ -170,7 +170,6 @@ function! airline#extensions#tabline#group_of_bufnr(tab_bufs, bufnr)
     else
       let group = 'airline_tabsel'
     endif
-    let s:current_modified = (group == 'airline_tabmod') ? 1 : 0
   else
     if g:airline_detect_modified && getbufvar(a:bufnr, '&modified')
       let group = 'airline_tabmod_unsel'

autoload/airline/extensions/tabline/buffers.vim
@@ -66,6 +66,7 @@ function! airline#extensions#tabline#buffers#get()
     endif
 
     let group = airline#extensions#tabline#group_of_bufnr(tab_bufs, nr)
+    let s:current_modified = (group == 'airline_tabmod') ? 1 : 0
     if s:buffer_idx_mode
       if len(s:number_map) > 0
         call b.add_section(group, s:spc . get(s:number_map, l:index, '') . '%(%{airline#extensions#tabline#get_buffer_name('.nr.')}%)' . s:spc)