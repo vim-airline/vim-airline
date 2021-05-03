vim-airline/vim-airline

commit 2e9df43962539e3a05e5571c63ccf6356451d46c

autocommands: check for existence of airline highlight group
Several extensions trigger custom actions. Make sure, those actions are
only performed, if airline is actually active and not temporarily
disabled using e.g. :AirlineToggle

autoload/airline/util.vim
@@ -174,6 +174,10 @@ function! airline#util#has_custom_scm()
 endfunction
 
 function! airline#util#doautocmd(event)
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   exe printf("silent doautocmd %s User %s", s:nomodeline, a:event)
 endfunction
 

autoload/airline/extensions/ale.vim
@@ -128,6 +128,10 @@ function! airline#extensions#ale#init(ext)
 endfunction
 
 function! s:ale_refresh()
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   if get(g:, 'airline_skip_empty_sections', 0)
     exe ':AirlineRefresh!'
   endif

autoload/airline/extensions/branch.vim
@@ -325,6 +325,10 @@ endfunction
 
 function! s:reset_untracked_cache(shellcmdpost)
   " shellcmdpost - whether function was called as a result of ShellCmdPost hook
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   if !g:airline#init#vim_async && !has('nvim')
     if a:shellcmdpost
       " Clear cache only if there was no error or the script uses an
@@ -346,11 +350,17 @@ function! s:reset_untracked_cache(shellcmdpost)
   endfor
 endfunction
 
+function! s:sh_autocmd_handler()
+  if exists('#airline')
+    unlet! b:airline_head b:airline_do_mq_check
+  endif
+endfunction
+
 function! airline#extensions#branch#init(ext)
   call airline#parts#define_function('branch', 'airline#extensions#branch#get_head')
 
-  autocmd ShellCmdPost,CmdwinLeave * unlet! b:airline_head b:airline_do_mq_check
-  autocmd User AirlineBeforeRefresh unlet! b:airline_head b:airline_do_mq_check
+  autocmd ShellCmdPost,CmdwinLeave * call s:sh_autocmd_handler()
+  autocmd User AirlineBeforeRefresh call s:sh_autocmd_handler()
   autocmd BufWritePost * call s:reset_untracked_cache(0)
   autocmd ShellCmdPost * call s:reset_untracked_cache(1)
 endfunction

autoload/airline/extensions/cursormode.vim
@@ -107,6 +107,10 @@ augroup airline#extensions#cursormode
 augroup END
 
 function! airline#extensions#cursormode#activate()
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   let s:color_map = s:get_color_map()
   call airline#extensions#cursormode#set()
 endfunction

autoload/airline/extensions/fugitiveline.vim
@@ -43,6 +43,12 @@ function! airline#extensions#fugitiveline#bufname()
   endif
 endfunction
 
+function! s:sh_autocmd_handler()
+  if exists('#airline')
+    unlet! b:fugitive_name
+  endif
+endfunction
+
 function! airline#extensions#fugitiveline#init(ext)
   if exists("+autochdir") && &autochdir
     " if 'acd' is set, vim-airline uses the path section, so we need to redefine this here as well
@@ -50,6 +56,6 @@ function! airline#extensions#fugitiveline#init(ext)
   else
     call airline#parts#define_raw('file', '%<%{airline#extensions#fugitiveline#bufname()}%m')
   endif
-  autocmd ShellCmdPost,CmdwinLeave * unlet! b:fugitive_name
-  autocmd User AirlineBeforeRefresh unlet! b:fugitive_name
+  autocmd ShellCmdPost,CmdwinLeave * call s:sh_autocmd_handler()
+  autocmd User AirlineBeforeRefresh call s:sh_autocmd_handler()
 endfunction

autoload/airline/extensions/languageclient.vim
@@ -35,6 +35,10 @@ function! s:record_diagnostics(state)
 endfunction
 
 function! s:get_diagnostics()
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   call LanguageClient#getState(function("s:record_diagnostics"))
 endfunction
 

autoload/airline/extensions/lsp.vim
@@ -88,6 +88,10 @@ endfunction
 let s:timer = 0
 let s:ignore_time = 0
 function! airline#extensions#lsp#update() abort
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   if reltimefloat(reltime()) - s:ignore_time >=
         \ get(g:, 'airline#extensions#lsp#progress_skip_time', 0.3)
         \ || len(s:lsp_progress) == 0

autoload/airline/extensions/po.vim
@@ -43,6 +43,10 @@ function! airline#extensions#po#shorten()
 endfunction
 
 function! airline#extensions#po#on_winenter()
+  if !exists('#airline')
+    " airline disabled
+    return
+  endif
   " only reset cache, if the window size changed
   if get(b:, 'airline_winwidth', 0) != airline#util#winwidth()
     let b:airline_winwidth = airline#util#winwidth()
@@ -51,11 +55,17 @@ function! airline#extensions#po#on_winenter()
   endif
 endfunction
 
+function! s:autocmd_handler()
+  if exists('#airline')
+    unlet! b:airline_po_stats
+  endif
+endfunction
+
 function! airline#extensions#po#apply(...)
   if &ft ==# 'po'
     call airline#extensions#prepend_to_section('z', '%{airline#extensions#po#stats()}')
     " Also reset the cache variable, if a window has been split, e.g. the winwidth changed
-    autocmd airline BufWritePost * unlet! b:airline_po_stats
+    autocmd airline BufWritePost * call s:autocmd_handler()
     autocmd airline WinEnter * call airline#extensions#po#on_winenter()
   endif
 endfunction

commit 7a87615cbe1ec78678fd1023faeecea2c5e0b077

Fire AirlineToggledOff autocmd even if airline is disabled
Fixes #2377.  This regression was introduced in 2e9df43.

autoload/airline/util.vim
@@ -174,7 +174,7 @@ function! airline#util#has_custom_scm()
 endfunction
 
 function! airline#util#doautocmd(event)
-  if !exists('#airline')
+  if !exists('#airline') && a:event !=? 'AirlineToggledOff'
     " airline disabled
     return
   endif